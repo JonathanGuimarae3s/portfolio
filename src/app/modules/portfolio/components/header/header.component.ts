c38733ca9af6837536e2be6e9e1f1abacde6ba33
Jonathan  Guimarães <guimaraesjonathan92@gmail.com>
2024-02-06 22:08:03 -0300

    chore: Finalizar e publicar o portfólio Esta alteração marca o final do desenvolvimento do portfólio e inclui todas as atualizações e ajustes finais. O portfólio está pronto para ser publicado e compartilhado.

--- src/app/modules/portfolio/components/header/header.component.ts
+++ src/app/modules/portfolio/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { AnimationsComponent } from "../animations/animations.component";
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+    selector: 'app-header',
+    standalone: true,
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.scss',
+    imports: [AnimationsComponent]
 })
 export class HeaderComponent {
 
--- src/app/modules/portfolio/pages/home/home.component.ts
+++ src/app/modules/portfolio/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from '../../components/header/header.component';
 import { KnowledgeComponent } from '../../components/knowledge/knowledge.component';
 import { ExperienceComponent } from '../../components/experience/experience.component';
 import { ProjectsComponent } from '../../components/projects/projects.component';
+import { AnimationsComponent } from '../../components/animations/animations.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, KnowledgeComponent, ProjectsComponent, ExperienceComponent],
+  imports: [HeaderComponent, KnowledgeComponent, ProjectsComponent, ExperienceComponent,],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
